0b94d9d6473773135d59983a33014527fabe2cd4
bobidiboo <nrobert57@protonmail.com>
2020-02-27 12:24:18 +0100

    the input was always assuming that the user will not add the protocol identifier

--- add_tool.py
+++ add_tool.py
@@ -10,14 +10,17 @@ while True:
     we ask the user to enter an url
     """
 
-    inputurl = input('url: ')
+    url_input = input('url: ')
 
     """
     each input are added to the url list
     ( ctrl-c to leave )
     """
 
-    data.append(f'https://{inputurl}/')
+    if url_input[0:4] == 'http':
+        data.append(f'{url_input}/')
+    else: 
+        data.append(f'https://{url_input}/')
 
     with open('urls.json', 'w') as f:
         f.write(json.dumps(data, indent=4))
